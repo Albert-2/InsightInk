d7f7747bb1a868cf44ded035d83f90c263b14c0b
Bhushan <alberteinstein02.99792458>
2024-10-28 23:03:52 +0530

    reset link fixed

--- server/api/routes/passReset.js
+++ server/api/routes/passReset.js
@@ -30,9 +30,7 @@ router.post("/reset-password", async (req, res) => {
   const token = jwt.sign({ id: user._id }, secret, { expiresIn: "10m" });
   const id = user._id;
   // Construct the reset link
-  const resetLink = `${encodeURIComponent(
-    FRONTEND_URL
-  )}/forgetPass/${encodeURIComponent(token)}/${encodeURIComponent(id)}`;
+  const resetLink = `${FRONTEND_URL}/forgetPass/${token}/${id}`;
 
   // Send email with the reset link
   const mailOptions = {
